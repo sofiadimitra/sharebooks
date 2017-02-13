e3aea756c1427c9bcdf68fa3f2ce6978731b909f
SofiaDimitra <sofia_dimitra@hotmail.com>
2017-02-14 00:06:25 +0200

    Add ability to reserve books

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :books
+	root 'books#index'
+  resources :books do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	  member do
+			post 'reserve', 'unreserve'
+	  end
+	end
 end
